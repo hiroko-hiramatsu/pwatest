bff069787a574a0d5ed11766a9d71d712717a095
hiroko-hiramatsu <63827097+hiroko-hiramatsu@users.noreply.github.com>
2020-06-30 15:35:56 +0900

    update

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+// ファイルのキャッシュ
+workbox.precaching.precacheAndRoute([
+    {
+      url: '/index.html',
+      revision: '1'
+    },
+    {
+      url: '/style.css',
+      revision: '1'
+    },
+    { 
+  　url: '/app.js',
+      revision: '1'
+    },
+  ]);
+
 window.addEventListener("load", function() {
     document.getElementById("hello").addEventListener("click", function() {
         document.getElementById("message").innerHTML += "Hello, ";
